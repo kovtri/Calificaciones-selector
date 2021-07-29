d6fc4f8b13c9f0e95969adf9a12206734e8da119
kovtri <kigsports23@gmail.com>
2021-07-29 04:59:31 +0200

    Se agrega el Proyecto

---
+++ codigo.js
@@ -0,0 +1,25 @@
+
+calificaciones = prompt("Cual es tu calificacion?");
+
+
+
+if (calificaciones >= 0 && calificaciones <= 3){
+	alert("Muy deficiente");
+}
+else if (calificaciones >= 3 && calificaciones <= 5){
+	alert("Insuficiente");
+}
+else if (calificaciones >= 5 && calificaciones <= 6){
+	alert("Suficiente");
+}
+else if (calificaciones >= 6 && calificaciones <= 7){
+	alert("Bien");
+}
+else if (calificaciones >= 7 && calificaciones <= 9){
+	alert("Notable");
+}
+else if (calificaciones >= 9 && calificaciones <= 10){
+	alert("Sobresaliente");
+}
+
+
